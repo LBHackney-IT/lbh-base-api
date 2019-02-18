2bda56dc85e49af8f73cb401d259555df3e243be
Miguel Saitz <miguel.saitz@hackney.gov.uk>
2019-02-18 13:22:40 +0000

    add tests project

---
+++ transactions-api.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Prime.UnitTests.Services
+{
+    [TestFixture]
+    public class FakeTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
