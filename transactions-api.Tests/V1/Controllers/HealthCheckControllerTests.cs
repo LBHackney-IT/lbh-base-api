9301a5fa9e76590508fb20ec6cd189d7b040b597
cob16 <cormac.brady@hotmail.co.uk>
2019-02-19 16:56:01 +0000

    add basic healthcheck controller

---
+++ transactions-api.Tests/V1/Controllers/HealthCheckControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace transactions_api.Controllers.V1
+{
+
+    [TestFixture]
+    public class HealthCheckControllerTests
+    {
+        private HealthCheckController _classUnderTest;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _classUnderTest = new HealthCheckController();
+        }
+
+        [Test]
+        public void ReturnsResponseWithStatus()
+        {
+            var response = _classUnderTest.HealthCheck() as OkObjectResult;
+
+            Assert.NotNull(response);
+            Assert.AreEqual(response.StatusCode, 200);
+            Assert.AreEqual(new Dictionary<string, object> {{"success", true}}, response.Value);
+
+        }
+    }
+}
---
+++ transactions-api/V1/Controllers/HealthCheckController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace transactions_api.Controllers.V1
+{
+    [Route("api/v1")]
+    [ApiController]
+    [Produces("application/json")]
+    public class HealthCheckController : BaseController
+    {
+        [HttpGet]
+        [Route("ping")]
+        [ProducesResponseType(typeof(Dictionary<string, bool>), 200)]
+        public IActionResult HealthCheck()
+        {
+            var result = new Dictionary<string, bool> {{"success", true}};
+
+            return Ok(result);
+        }
+    }
+}
