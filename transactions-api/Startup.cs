b7026c823cd90bacc417e6eff4859d36ff172192
cob16 <cormac.brady@hotmail.co.uk>
2019-02-19 10:31:10 +0000

    remove UseHttpsRedirection

--- transactions-api/Startup.cs
+++ transactions-api/Startup.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace transactions_api
 {
@@ -40,7 +33,6 @@ namespace transactions_api
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
