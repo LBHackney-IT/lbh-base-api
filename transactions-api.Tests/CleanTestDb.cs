5ed59ec9fb7123aee7c505ddae44da52eac58c04
cob16 <public+github@cormacbrady.info>
2019-02-27 10:37:49 +0000

    use test db in build

--- transactions-api.Tests/CleanTestDb.cs
+++ transactions-api.Tests/CleanTestDb.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using UnitTests.V1.Infrastructure;
@@ -12,7 +13,10 @@ namespace UnitTests
         public void RunBeforeAnyTests()
         {
             DbContextOptionsBuilder builder = new DbContextOptionsBuilder();
-            builder.UseSqlServer(@"Server=localhost;Database=uhsimulator;User Id='sa';Password='Rooty-Tooty';");
+
+            string TEST_UH_URL = Environment.GetEnvironmentVariable("TEST_UH_URL");
+            builder.UseSqlServer(TEST_UH_URL);
+
             _uhContext = new UhContext(builder.Options);
             _uhContext.Database.BeginTransaction();
         }
