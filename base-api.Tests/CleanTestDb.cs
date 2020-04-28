d76d147fcfdefc8da2bf040f34a1c8a864f020d6
Ben Reynolds-Carr <ben.reynolds-carr@hackney.gov.uk>
2020-04-28 15:43:35 +0100

    database environemnt variable file created

--- base-api.Tests/CleanTestDb.cs
+++ base-api.Tests/CleanTestDb.cs
@@ -16,12 +16,13 @@ namespace UnitTests
 
             // To run database tests locally (eg. via Visual Studio) the TEST_DB_URL environment variable will need to be populated with
             // @"Host=localhost;Database=entitycore;Username=postgres;Password=mypassword";
-            string TEST_DB_URL = @"Host=stub-test-db;Database=entitycore;Username=postgres;Password=mypassword";
+            string TEST_DB_URL = Environment.GetEnvironmentVariable("TEST_DB_URL") ??
+                                 @"Host=stub-test-db;Database=entitycore;Username=postgres;Password=mypassword";
             // Note: The Host name needs to be the name of the stub database docker-compose service, in order to run tests via Docker
 
             // Delete as appropriate
             // If using SQL:
-            // builder.UseSqlServer(TEST_DB_URL);
+            builder.UseSqlServer(TEST_DB_URL);
 
             // If using Postgres:
             builder.UseNpgsql(TEST_DB_URL);
