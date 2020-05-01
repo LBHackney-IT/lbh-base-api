eb746d529abba5ead297ddfb1cc065d21e105bfb
Liudvikas Taluntis <liudvikast@gmail.com>
2020-05-01 11:17:23 +0100

    Powershell script - Added Files renaming.
    Files have to be renamed as well, otherwise all the assembly references that were already modified won't find the files in question.

--- Renamer.ps1
+++ Renamer.ps1
@@ -14,4 +14,10 @@ Get-ChildItem -File -Recurse -exclude *.ps1 | % {
         $contents -replace 'base-api', $apiName -replace 'base_api', $apiName | Set-Content $_.PSPath
         Write-Host $("'{0}': contents changed." -f $fileName)
     }
+    
+    if ($fileName -match 'base-api') {
+        $newName = $_.Name -replace 'base-api', $apiName
+        Rename-Item -Path $_.PSPath -NewName $newName
+        Write-Host $("File renamed from '{0}' to '{1}'." -f $fileName, $newName)
+    }
 }
