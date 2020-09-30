b2f72a256cff1ab0f94ef0b7da121d49002f043c
Alex <alex.demetriou@hackney.gov.uk>
2020-09-30 16:01:07 +0100

    add *nix renaming script

---
+++ rename.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo -e "Input: '$1'\n"
+
+SNAKE=$(echo $1 | sed "s/\([A-Z]\)/_\L\1/g;s/^_//;s/-/_/g")
+KEBAB=$(echo $SNAKE | sed -r "s/_/-/g")
+PASCAL=$(echo $SNAKE | sed -r "s/(^|_)([a-z])/\U\2/g")
+
+echo "Renaming all 'base_api' -> '$SNAKE'"
+echo "Renaming all 'base-api' -> '$KEBAB'"
+echo "Renaming all 'BaseApi' -> '$PASCAL'"
+
+echo -e "\nRenaming in $PWD.\n"
+read -p "Does this sound OK? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Cancelled."
+  exit 1
+fi
+
+# folder
+find . -type d -not -path '*/\.git/*' | sed -e "p;s/base_api/$SNAKE/" | xargs -n2 mv
+find . -type d -not -path '*/\.git/*' | sed -e "p;s/base-api/$KEBAB/" | xargs -n2 mv
+find . -type d -not -path '*/\.git/*' | sed -e "p;s/BaseApi/$PASCAL/" | xargs -n2 mv
+# file names
+find . -type f -not -path '*/\.git/*' | sed -e "p;s/base_api/$SNAKE/" | xargs -n2 mv
+find . -type f -not -path '*/\.git/*' | sed -e "p;s/base-api/$KEBAB/" | xargs -n2 mv
+find . -type f -not -path '*/\.git/*' | sed -e "p;s/BaseApi/$PASCAL/" | xargs -n2 mv
+# strings
+find . -type f -not -path '*/\.git/*' -exec sed -i "s/base_api/$SNAKE/g" {} \;
+find . -type f -not -path '*/\.git/*' -exec sed -i "s/base-api/$KEBAB/g" {} \;
+find . -type f -not -path '*/\.git/*' -exec sed -i "s/BaseApi/$PASCAL/g" {} \;
