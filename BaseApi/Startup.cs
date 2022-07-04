f5db09768396160578a45bb04c26aba57e4e0631
humulla <43747248+humulla@users.noreply.github.com>
2022-07-04 14:25:17 +0100

    Upgraded net core version to 6.0

--- BaseApi/Startup.cs
+++ BaseApi/Startup.cs
@@ -53,9 +53,9 @@ namespace BaseApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-                .AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            //services
+            //    .AddMvc()
+            //    .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddApiVersioning(o =>
             {
                 o.DefaultApiVersion = new ApiVersion(1, 0);
